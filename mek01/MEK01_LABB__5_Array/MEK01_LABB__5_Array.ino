0ad896698c6f3c1178d27cacd21c35b80c09087e
Gusta-V1 <81166154+Gusta-V1@users.noreply.github.com>
2021-10-07 11:32:47 +0200

    Array CC, CA

---
+++ mek01/MEK01_LABB__5_Array/MEK01_LABB__5_Array.ino
@@ -0,0 +1,41 @@
+/*
+Input: Read potentiometer values
+Logic: Map potentiometer integers to LED compatible integers
+Output: Send value of mapped potentiometer to corresponding led pin
+*/
+
+//Variables for potentiometer values
+int pot[] = {0, 0, 0};
+
+//Variables for LED compatible remapped potentiometer values
+int mappedPot[] = {0, 0, 0};
+
+//Assign LED pins
+int ledPins[] = {6, 10, 11};
+
+//Assign potentiometer pins
+int potPins[] = {A1, A2, A3};
+
+void setup() {
+    for(int i = 0; i < 3; i++){ //For loop with range 3
+    pinMode(ledPins[i], OUTPUT); //Set all led pins to output mode
+  }
+  Serial.begin(115200); //Set BAUD rate
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++){ //For loop with range 3
+
+    //Logic
+    pot[i] = analogRead(potPins[i]); //Step through potentiometers and read each
+    mappedPot[i] = map(pot[i], 0, 1023, 0, 255); //Map potentiometer values to LED compatible integers
+    
+    //Output:
+    analogWrite(ledPins[i], mappedPot[i]); //Output mapped values to LED
+    Serial.print(pot[i]); //Print potentiometer setting
+    Serial.print("\t");  //Space column period
+    Serial.print(Mappedpot[i]); //Print LED setting
+    Serial.print("\t"); //Space between column
+  }
+  Serial.print("\n"); //Break line
+}
