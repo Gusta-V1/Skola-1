3893cabf89cfdb1b90a2c9da7c3b181353e1bdf2
Gusta-V1 <81166154+Gusta-V1@users.noreply.github.com>
2021-10-14 10:28:02 +0200

    Create MEK01_LABB_6.ino

---
+++ mek01/MEK01_LABB_6/MEK01_LABB_6.ino
@@ -0,0 +1,18 @@
+/*
+Input: Use analog read to measure voltage between photocell and resistor
+Logic: If photocell read value corresponds to no light detected then set LED to max. If light is detected set LED to off.
+Output: Send LED value to LED, write to serial monitor.
+*/
+
+int photo = 0;
+bool LEDstate = 0;
+
+void setup()
+{
+    pin
+}
+
+void loop()
+{
+    
+}
