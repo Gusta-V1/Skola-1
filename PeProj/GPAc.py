d2ccf025269a82b2a46bd25bd9416687ae7e3c2d
Gusta-V1 <81166154+Gusta-V1@users.noreply.github.com>
2025-07-08 19:57:24 +0200

    start A0

---
+++ PeProj/GPAc.py
@@ -0,0 +1,15 @@
+
+class course:
+    
+    def __init__(self, name, grade, gradePoint):
+        self.name = name
+        self.grade = grade
+        self.gradePoint = gradePoint
+
+    def setup(self,name, grade)
+        
+        map(A:F, 1:5)
+
+
+def create():
+    pass
